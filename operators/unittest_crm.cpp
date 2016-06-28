10f0dd76d667afbf70ec9842528a88ee7e1728b3
Martin Licht <licht@ins.uni-bonn.de>
2016-06-28 22:49:37 +0200

    working unittest for CRM

--- operators/unittest_crm.cpp
+++ operators/unittest_crm.cpp
@@ -6,6 +6,7 @@
 #include "../basic.hpp"
 #include "floatvector.hpp"
 #include "scalingoperator.hpp"
+#include "sparsematrix.hpp"
 #include "crm.hpp"
 
 
@@ -15,15 +16,56 @@ int main()
 {
 	cout << "Unit Test for Conjugate Residual Method" << endl;
 	
-	ScalingOperator S( 10, 3.141 );
-	ConjugateResidualMethod CRM(S);
+	{
+		
+		cout << "First Something Simple" << endl;
+		
+		ScalingOperator S( 10, 3.141 );
+		ConjugateResidualMethod CRM(S);
 
-	FloatVector rhs(10), x(10);
-	x.random(); rhs.zero();
+		FloatVector rhs(10), x(10);
+		x.random(); rhs.zero();
+		
+		cout << x << endl;
+		cout << CRM << endl;
+		CRM.solve( x, rhs );
 	
-	cout << x << endl;
+	}
 	
-	CRM.solve( x, rhs );
+
+	{
+		
+		cout << "Now something more complicated." << endl;
+		
+		int dimension = 1000;
+		
+		FloatVector x( dimension );
+		for( int p = 0; p < dimension; p++ )
+			x.setentry( p, 3. + p * 5. );
+		
+		SparseMatrix M( dimension, dimension );
+		for( int i = 0; i < dimension; i++ ){
+			if( i-1 >= 0 ) 
+				M.addentry( i, i-1, 1.25 );
+			if( i+1 < dimension ) 
+				M.addentry( i, i+1, 1.25 );
+			M.addentry( i, i, 11.234 );
+		}
+		M.sortentries();
+		
+		FloatVector b = M * x;
+		
+		FloatVector y( dimension );
+		y.random();
+		
+		cout << M << endl;
+		
+		ConjugateResidualMethod CRM(M);
+		
+		CRM.solve(y,b);
+
+		
+	}
 	
 	cout << "Finished Unit Test" << endl;
 
