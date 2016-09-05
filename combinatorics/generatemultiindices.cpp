3dfb29c1b876aebc453ed7512d5d6c461e638458
Martin Licht <licht@ins.uni-bonn.de>
2016-09-05 18:05:56 +0200

    generation of multiindices

--- combinatorics/generatemultiindices.cpp
+++ combinatorics/generatemultiindices.cpp
@@ -6,8 +6,8 @@
 #include <iterator>
 
 #include "../basic.hpp"
-// #include "indexrange.hpp"
-// #include "indexmap.hpp"
+#include "indexrange.hpp"
+#include "indexmap.hpp"
 #include "multiindex.hpp"
 
 
@@ -15,7 +15,28 @@ std::vector<MultiIndex> generateMultiIndices( const IndexRange& ir, int degree )
 {
     assert( degree >= 0 );
     std::vector<MultiIndex> ret;
-    // TODO: Implement 
+    
+    int max_candidate = integerpower( degree+1, ir.getlength() );
+    int min_index = ir.min();
+    int max_index = ir.max();
+    
+    for( int candidate = degree; candidate < max_candidate; candidate++ ) {
+        
+        MultiIndex mi_candidate( ir );
+        int candidate_copy = candidate;
+        
+        for( int i = min_index; i <= max_index; i++ ) {
+            mi_candidate[ i ] = candidate_copy % ( degree+1 );
+            candidate_copy /= degree+1;
+        }
+        
+        if( mi_candidate.absolute() == degree )
+            ret.push_back( mi_candidate );
+        
+    }
+    
+    assert( ret.size() == binomial<int>( ir.getlength()-1 + degree, ir.getlength()-1 ) );
+    
     return ret;
 }
 
