184dd4ec2364d48b824a70703791e48b96abcd94
Martin Licht <licht@ins.uni-bonn.de>
2017-04-06 00:48:45 +0200

    updated some unit tests

--- unittests/test.dense.cholesky.cpp
+++ unittests/test.dense.cholesky.cpp
@@ -14,14 +14,16 @@ using namespace std;
 
 int main()
 {
-	cout << "Unit Test for Cholesky decomposition" << endl;
-	
-	std::cout.precision(5);
-	std::cout.setf( std::ios::fixed, std:: ios::floatfield );
-	std::cout << std::showpos;
-	
-	DenseMatrix A(3,3);
-	
+    cout << "Unit Test for Cholesky decomposition" << endl;
+    
+    std::cout.precision(5);
+    std::cout.setf( std::ios::fixed, std:: ios::floatfield );
+    std::cout << std::showpos;
+    
+    {
+        
+        DenseMatrix A(3,3);
+        
         A(0,0) =   4; A(0,1) =  12; A(0,2) = -16; 
         A(1,0) =  12; A(1,1) =  37; A(1,2) = -43; 
         A(2,0) = -16; A(2,1) = -43; A(2,2) =  98; 
@@ -34,7 +36,26 @@ int main()
         
         cout << "Product matrix:" << L * Transpose(L) << endl;
         
-        cout << "Finished Unit Test" << endl;
+    }
+      
+    {
+    
+        A.zeromatrix();
+        for( int s = 0; s < D; s++ )
+        for( int t = 0; t < D; t++ )
+            A(s,t) = 3 * kronecker(s,t) - kronecker(s,t-1) - kronecker(s,t+1);
+        
+        DenseMatrix L = CholeskyDecomposition( A );
+        
+        cout << "Original matrix:" << A << endl;
+        
+        cout << "Factor matrix:" << L << endl;
+        
+        cout << "Product matrix:" << L * Transpose(L) << endl;
+        
+    }
+    
+    cout << "Finished Unit Test" << endl;
 
-	return 0;
+    return 0;
 }
--- unittests/test.dense.matrixalgorithm.cpp
+++ unittests/test.dense.matrixalgorithm.cpp
@@ -5,6 +5,7 @@
 #include <iostream>
 #include "../basic.hpp"
 #include "../dense/dense.functions.hpp"
+#include "../dense/dense.simplesolver.hpp"
 #include "../dense/qr.factorization.hpp"
 #include "../dense/lu.factorization.hpp"
 #include "../dense/cholesky.hpp"
@@ -14,68 +15,50 @@ using namespace std;
 
 int main()
 {
-	cout << "Unit Test for Dense Matrix Module" << endl;
-	
-	std::cout.precision(5);
-	std::cout.setf( std::ios::fixed, std:: ios::floatfield );
-	std::cout << std::showpos;
-	
-	const int D = 8;
-	DenseMatrix A(D,D);
-	
+        cout << "Unit Test for Dense Matrix Module" << endl;
+        
+        std::cout.precision(5);
+        std::cout.setf( std::ios::fixed, std:: ios::floatfield );
+        std::cout << std::showpos;
+        
+        const int D = 4;
+        DenseMatrix A(D,D);
+        
         A.zeromatrix();
-	for( int s = 0; s < D; s++ )
-	for( int t = 0; t < D; t++ )
-		A(s,t) = 3 * kronecker(s,t) - kronecker(s,t-1) - kronecker(s,t+1);
-		
-	{
-		DenseMatrix Q(D,D), R(D,D);
-		
-		QRFactorization( A, Q, R );
-		
-		// for( int r = 0; r < D; r++ )
-		// for( int c = 0; c < D; c++ )
-			// cout << ;
-		
-		DenseMatrix Rinv = UpperTriangularInverse(R);
-		DenseMatrix Qt = Transpose(Q);
-		
-		cout << "Matrix A:" << A;
-		cout << "Matrix Q:" << Q;
-		cout << "Matrix R:" << R;
-		cout << "Matrix Q * R:" << Q * R;
-		cout << "Matrix Q^t:" << Qt;
-		cout << "Matrix Rinv:" << Rinv;
-		cout << "Matrix R * Rinv:" << R * Rinv;
-		cout << "Matrix Rinv * R:" << Rinv * R;
-		cout << "Matrix Q * Qinv:" << Q * Qt;
-		cout << "Matrix Qinv * Q:" << Qt * Q;
-		cout << "Matrix Rinv * Q^t * A:" << Rinv * Qt * A;
-		cout << "Matrix A * Rinv * Q^t:" << A * Rinv * Qt;
-
-		
-	}
-
-	{
-		
-                cout << endl << "Now for Cholesky" << endl;
+        for( int s = 0; s < D; s++ )
+        for( int t = 0; t < D; t++ )
+                A(s,t) = 3 * kronecker(s,t) - kronecker(s,t-1) - kronecker(s,t+1);
                 
-                DenseMatrix M = Transpose(A) * A;
-		M = M * M;
-		DenseMatrix R = CholeskyDecomposition( M );
-		DenseMatrix Rinv = UpperTriangularInverse(R);
-		
-		cout << "Matrix M:" << M;
-		cout << "Matrix R:" << R;
-		cout << "Matrix Rt * R:" << Transpose(R) * R;
-		cout << "Matrix Rinv * Rtinv:" << Rinv * Transpose(Rinv);
-		cout << "Matrix M * Rinv * Rtinv:" << M * Rinv * Transpose(Rinv);
-		cout << "Matrix Rinv * Rtinv * M:" << Rinv * Transpose(Rinv) * M;
+        {
+                DenseMatrix Q(D,D), R(D,D);
                 
-	}
-	
-	
-	cout << "Finished Unit Test" << endl;
+                QRFactorization( A, Q, R );
+                
+                // for( int r = 0; r < D; r++ )
+                // for( int c = 0; c < D; c++ )
+                        // cout << ;
+                
+                DenseMatrix Rinv = Inverse(R);
+                DenseMatrix Qt = Transpose(Q);
+                
+                cout << "Matrix A:" << A;
+                cout << "Matrix Q:" << Q;
+                cout << "Matrix R:" << R;
+                cout << "Matrix Q * R:" << Q * R;
+                cout << "Matrix Q^t:" << Qt;
+                cout << "Matrix Rinv:" << Rinv;
+                cout << "Matrix R * Rinv:" << R * Rinv;
+                cout << "Matrix Rinv * R:" << Rinv * R;
+                cout << "Matrix Q * Qinv:" << Q * Qt;
+                cout << "Matrix Qinv * Q:" << Qt * Q;
+                cout << "Matrix Rinv * Q^t * A:" << Rinv * Qt * A;
+                cout << "Matrix A * Rinv * Q^t:" << A * Rinv * Qt;
 
-	return 0;
+                
+        }
+
+        
+        cout << "Finished Unit Test" << endl;
+
+        return 0;
 }
