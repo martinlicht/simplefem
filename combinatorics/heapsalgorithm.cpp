38024b66be6c698fe3ec1b2ddfe6a80476282791
Martin Licht <licht@ins.uni-bonn.de>
2017-04-05 13:57:43 +0200

    added Heaps algorithm for permutation listing

---
+++ combinatorics/heapsalgorithm.cpp
@@ -0,0 +1,52 @@
+
+#include "heapsalgorithm.hpp"
+
+#include <algorithm>
+#include <vector>
+#include <iterator>
+
+#include "../basic.hpp"
+
+
+
+void HeapsAlgorithmInit( int& i, std::vector<int>& c, const std::vector<int>& a )
+{
+  i = 0;
+  assert( c.size() == a.size() );
+  for( int j = 0; j < c.size(); j++ ) c[i] = 0;
+}
+
+
+bool HeapsAlgorithmStep( int& i, std::vector<int>& c, std::vector<int>& a )
+{
+  const int n = c.size();
+  assert( c.size() == a.size() );
+  assert( 0 <= i && i <= n );
+  
+  while( i < n )
+  {
+    if( c[i] < i ) {
+      
+      if( i % 2 == 0 ) {
+        std::swap( a[  0 ], a[i] );
+      } else {
+        std::swap( a[c[i]], a[i] );
+      }
+      
+      c[i]++;
+      i = 0;
+      return true;
+      
+    } else {
+      
+      c[i] = 0;
+      i++;
+      
+    }
+  }
+  
+  return false;
+}
+
+
+
---
+++ combinatorics/heapsalgorithm.hpp
@@ -0,0 +1,26 @@
+#ifndef INCLUDEGUARD_HEAPS_ALGORITHM
+#define INCLUDEGUARD_HEAPS_ALGORITHM
+
+#include <vector>
+
+#include "../basic.hpp"
+
+
+/***************
+*** 
+***  Generate Permutations using Heaps algorithm
+***  
+***  Init - initiliazes the auxiliary data: i and c
+***  
+***  Step - Returns true if a has experienced a transposition
+***         Returns false if a has not been changed.
+***         Supposed to be used in a do-while loop.
+***  
+***************/
+
+void HeapsAlgorithmInit( int& i, std::vector<int>& c, const std::vector<int>& a );
+
+bool HeapsAlgorithmStep( int& i, std::vector<int>& c, std::vector<int>& a );
+
+
+#endif
