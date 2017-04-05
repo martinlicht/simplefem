e59a07241094ab1a3624b028dd22b031300fded7
Martin Licht <licht@ins.uni-bonn.de>
2017-04-05 21:17:23 +0200

    added header for mathematical constants

---
+++ constants.hpp
@@ -0,0 +1,39 @@
+#ifndef INCLUDEDGARD_CONSTANTS_HPP
+#define INCLUDEDGARD_CONSTANTS_HPP
+
+namespace Constants
+{
+  
+  const double pi
+    = 3.141592653589793238462643383279502884197;
+  
+  const double euler
+    = 2.718281828459045235360287471352662497757;
+  
+  /* feigenbaum bifurcation velocity */
+  const double feigenbaum_first
+    = 4.669201609102990671853203820466201617258;
+  
+  const double feigenbaum_second
+    = 2.502907875095892822283902873218215786381;
+    
+  const double goldenratio
+    = 1.618033988749894848204586834365638117720;
+  
+  const double gelfond
+    = 23.14069263277926900572908636794854738026;
+  
+  const double reci_fibonacci
+    = 3.359885666243177553172011302918927179688;
+  
+  const double levi
+    = 3.275822918721811159787681882;
+    
+  const double sirpinski
+    = 2.584981759579253217065893587383;
+    
+    
+    
+}
+
+#endif
