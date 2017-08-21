f516facb1d505b8be7962fac3a2d9ae7ebe1dbea
Martin Licht <licht@ins.uni-bonn.de>
2017-08-20 18:26:38 -0700

    added test module

---
+++ unittests/test.mesh.simplicialND.cpp
@@ -0,0 +1,32 @@
+
+
+/**/
+
+#include <iostream>
+#include <fstream>
+
+#include "../basic.hpp"
+#include "../mesh/coordinates.hpp"
+#include "../mesh/mesh.simplicialND.hpp"
+
+
+using namespace std;
+
+int main()
+{
+	cout << "Unit Test for N-dimensional simplicial mesh" << endl;
+	
+        MeshSimplicialND M = UnitSquare();
+        
+        cout << "Check" << endl;
+        
+        M.check();
+	
+        cout << "Check done" << endl;
+        
+        cout << M << endl;
+        
+        cout << "Finished Unit Test" << endl;
+
+	return 0;
+}
