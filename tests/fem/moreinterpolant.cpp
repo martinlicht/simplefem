a41322669e3e89614de046937e745a462004b8f1
Martin Licht <mlicht@ucsd.edu>
2019-09-08 22:06:22 +0100

    add interpolation unit test

---
+++ tests/fem/moreinterpolant.cpp
@@ -0,0 +1,102 @@
+
+
+/**/
+
+#include <iostream>
+#include <fstream>
+
+#include "../../basic.hpp"
+#include "../../mesh/coordinates.hpp"
+#include "../../mesh/mesh.simplicial2D.hpp"
+#include "../../mesh/examples2D.hpp"
+#include "../../mesh/examples3D.hpp"
+#include "../../fem/local.polynomialmassmatrix.hpp"
+#include "../../fem/global.massmatrix.hpp"
+#include "../../fem/global.diffmatrix.hpp"
+#include "../../fem/global.lagrangeincl.hpp"
+#include "../../fem/foo.hpp"
+
+
+using namespace std;
+
+int main()
+{
+        cout << "Unit Test for Interpolation in FEEC" << endl;
+        
+        cout << "2D Calculations" << endl;
+        
+        {
+
+            MeshSimplicial2D M = UnitDisk(3);
+            
+            cout << "... mesh done" << endl;
+        
+            auto scalarfield = [](const FloatVector& vec) -> FloatVector{
+                assert( vec.getdimension() == 2 );
+                return FloatVector({ sqrt( vec[0]*vec[0] + vec[1]*vec[1] ) });
+                return FloatVector({ 1 + vec[0] + vec[1] * vec[1] });
+            };
+            
+            auto vectorfield = [](const FloatVector& vec) -> FloatVector{
+                assert( vec.getdimension() == 2 );
+                return FloatVector({ 1 + vec[0], vec[1] * vec[1] });
+            };
+            
+            for( int r = 0; r <  3; r++ )
+                FloatVector results = Interpolation( M, M.getinnerdimension(), 0, r, scalarfield );
+            
+            for( int r = 0; r <  3; r++ )
+                FloatVector results = Interpolation( M, M.getinnerdimension(), 1, r, vectorfield );
+            
+            for( int r = 0; r <  3; r++ )
+                FloatVector results = Interpolation( M, M.getinnerdimension(), 2, r, scalarfield );
+
+        }
+        
+        cout << "3D Calculations" << endl;
+        
+        {
+
+            MeshSimplicial3D M = UnitSimplex3D();
+//             M.uniformrefinement();
+            
+            cout << "... mesh done" << endl;
+        
+            auto scalarfield = [](const FloatVector& vec) -> FloatVector{
+                assert( vec.getdimension() == 3 );
+                return FloatVector({ sqrt( vec[0]*vec[0] + vec[1]*vec[2] ) });
+            };
+            
+            auto vectorfield = [](const FloatVector& vec) -> FloatVector{
+                assert( vec.getdimension() == 3 );
+                return FloatVector({ 1 + vec[0], vec[1] * vec[1], 2. * vec[2] });
+            };
+            
+            int Rmax = 2;
+            
+            cout << "... k=0" << endl;
+        
+            for( int r = 0; r <= Rmax; r++ )
+                FloatVector results = Interpolation( M, M.getinnerdimension(), 0, r, scalarfield );
+            
+            cout << "... k=1" << endl;
+        
+            for( int r = 0; r <= Rmax; r++ )
+                FloatVector results = Interpolation( M, M.getinnerdimension(), 1, r, vectorfield );
+            
+            cout << "... k=2" << endl;
+        
+            for( int r = 0; r <= Rmax; r++ )
+                FloatVector results = Interpolation( M, M.getinnerdimension(), 2, r, vectorfield );
+
+            cout << "... k=3" << endl;
+        
+            for( int r = 0; r <= Rmax; r++ )
+                FloatVector results = Interpolation( M, M.getinnerdimension(), 3, r, scalarfield );
+
+        }
+        
+        cout << "Finished Unit Test" << endl;
+        
+        return 0;
+}
