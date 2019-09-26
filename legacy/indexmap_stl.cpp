cb3d80a97849a1b2b867144e14c49db1361d419d
Martin Licht <mlicht@ucsd.edu>
2019-09-26 13:35:44 +0100

    add IndexMap with STL to legacy folder

---
+++ legacy/indexmap_stl.cpp
@@ -0,0 +1,314 @@
+
+#include "indexmap.hpp"
+
+#include <iostream>
+
+#include "indexrange.hpp"
+
+
+
+
+// IndexMap::IndexMap( const IndexRange& range )
+// : IndexMap( range, range )
+// {
+//     check();
+// }
+// 
+// IndexMap::IndexMap( const IndexRange& from, const IndexRange& to )
+// : src(from), dest(to), values( std::max( src.max() - src.min() + 1, 0 ), to.min() )
+// {
+//     if( src.max() >= src.min() )
+//       std::cout << "Index Map initialized without actual values" << std::endl;
+//     src.check();
+//     to.check();
+//     check();
+// }
+
+IndexMap::IndexMap( const IndexRange& range, const std::vector<int>& values )
+: IndexMap( range, range, values )
+{
+    check();
+}
+
+IndexMap::IndexMap( const IndexRange& from, const IndexRange& to, const std::vector<int>& values )
+: src(from), dest(to), values(values)
+{
+    check();
+}
+
+IndexMap::IndexMap( const IndexRange& range, const std::function<int(int)>& generator )
+: IndexMap( range, range, generator )
+{
+    check();
+}
+
+IndexMap::IndexMap( const IndexRange& from, const IndexRange& to, const std::function<int(int)>& generator )
+: src(from), dest(to), values()
+{
+    if( from.isempty() ) {
+    
+        values.resize(0);
+    
+    } else {
+    
+        values.reserve( std::max( src.max() - src.min() + 1, 0 ) );
+        for( int e = src.min(); e <= src.max(); e++ )
+            // values.at( src.element2position(e) ) = generator(e);
+            values.emplace_back( generator(e) );
+        
+    }
+    check();
+}
+
+IndexMap::IndexMap( const IndexRange& range, const std::initializer_list<int>& values )
+: IndexMap( range, std::vector<int>(values) )
+{
+  check();
+}
+
+IndexMap::IndexMap( const IndexRange& from, const IndexRange& to, const std::initializer_list<int>& values )
+: IndexMap( from, to, std::vector<int>(values) )
+{
+  check();
+}
+
+
+        
+        
+
+
+
+void IndexMap::check() const 
+{ 
+    #ifdef NDEBUG
+    return;
+    #endif
+    
+    src.check();
+    dest.check();
+    
+    assert( getSourceRange().cardinality() == values.size() );
+    
+    if( values.size() > 0 ) {
+        
+        assert( ! getSourceRange().isempty() );
+        
+        assert( std::max( src.max() - src.min() + 1, 0 ) == values.size() );
+    
+        assert( ! getDestRange().isempty() );
+        
+        for( int a = src.min(); a <= src.max(); a++ )
+            assert( dest.contains( values.at( a - src.min() ) ) );
+        
+    } else {
+        
+        assert( getSourceRange().isempty() );
+        
+    }
+        
+}
+
+void IndexMap::print( std::ostream& os, bool embellish ) const 
+{
+    if( embellish ) {
+        os << "From" << std::endl << getSourceRange();
+        os << "To"   << std::endl << getDestRange();
+        for( int i : getSourceRange() )
+            os << i << " -> " << at( i ) << std::endl;
+    } else {
+        os << getSourceRange() << " " << getDestRange();
+        for( int i : getSourceRange() )
+            os << " " << at( i );
+    }
+}
+
+const IndexRange& IndexMap::getSourceRange() const 
+{
+    return src;
+}
+
+const IndexRange& IndexMap::getDestRange() const 
+{
+    return dest;
+}
+
+const std::vector<int>& IndexMap::getvalues() const 
+{
+    return values;
+}
+
+
+
+
+int& IndexMap::at( int i )
+{
+    check();
+    assert( !src.isempty() );
+    assert( src.contains(i) );
+    assert( 0 <= i - src.min() );
+    assert( i - src.min() < values.size() );
+    return values.at( i - src.min() );
+}
+
+const int& IndexMap::at( int i ) const
+{
+    check();
+    assert( !src.isempty() );
+    assert( src.contains(i) );
+    assert( 0 <= i - src.min() );
+    assert( i - src.min() < values.size() );
+    return values.at( i - src.min() );
+}
+
+int& IndexMap::operator[]( int i )
+{
+    check();
+    assert( !src.isempty() );
+    assert( src.contains(i) );
+    assert( 0 <= i - src.min() );
+    assert( i - src.min() < values.size() );
+    return values[ i - src.min() ];
+}
+
+const int& IndexMap::operator[]( int i ) const
+{
+    check();
+    assert( !src.isempty() );
+    assert( src.contains(i) );
+    assert( 0 <= i - src.min() );
+    assert( i - src.min() < values.size() );
+    return values[ i - src.min() ];
+}
+
+
+
+
+bool IndexMap::isempty() const 
+{
+    check();
+    return getSourceRange().isempty();
+}
+
+
+bool IndexMap::isinjective() const 
+{
+    check();
+    
+    if( getSourceRange().isempty() )
+        return true;
+    
+    for( int a = src.min(); a <= src.max(); a++ )
+        for( int b = src.min(); b <= src.max(); b++ )
+            if( a != b && values.at( a - src.min() ) == values.at( b - src.min() ) )
+                return false;
+    
+    return true;
+}
+
+bool IndexMap::issurjective() const 
+{
+    check();
+    
+    if( getSourceRange().isempty() )
+        return true;
+    
+    for( int a = dest.min(); a <= dest.max(); a++ ) 
+    {
+        bool flag = false;
+        for( int b = src.min(); b <= src.max(); b++ )
+            if( values.at( b - src.min() ) == a )
+                flag = true;
+        if( !flag )
+            return false;
+    }
+    
+    return true;
+}
+
+bool IndexMap::isbijective() const 
+{
+    check();
+    return isinjective() && issurjective();
+}
+
+bool IndexMap::isstrictlyascending() const
+{
+    check();
+    
+    if( getSourceRange().isempty() )
+        return true;
+    
+    for( int a = src.min(); a < src.max(); a++ )
+        if( values.at( a - src.min() ) >= values.at( a - src.min() + 1 ) )
+            return false;
+    
+    return true;
+}
+
+bool IndexMap::rangecontains( int p ) const
+{
+    check();
+    assert( getDestRange().contains(p) );
+    for( int i : src )
+        if( at(i) == p )
+            return true;
+    return false;
+} 
+
+int IndexMap::preimageof( int p ) const
+{
+    check();
+    assert( getDestRange().contains(p) );
+    for( int i : src )
+        if( at(i) == p )
+            return i;
+    unreachable();
+} 
+        
+bool IndexMap::comparablewith( const IndexMap& im ) const
+{
+    check();
+    return getSourceRange() == im.getSourceRange() && getDestRange() == im.getDestRange();
+} 
+
+bool IndexMap::equals( const IndexMap& im ) const
+{
+    check();
+    im.check();
+    assert( comparablewith( im ) );
+    return getvalues() == im.getvalues();
+} 
+
+bool IndexMap::less( const IndexMap& im ) const
+{
+    check();
+    im.check();
+    assert( comparablewith( im ) );
+    return getvalues() < im.getvalues();
+} 
+
+
+
+// IndexMap IndexMap::skip( int i ) const 
+// {
+//     check();
+//     assert( src.contains(i) );
+//     IndexMap ret( *this );
+//     ret.src = IndexRange( src.min(), src.max() - 1 );
+//     ret.values.erase( ret.values.begin() + ( i - src.min() ) );
+//     ret.check();
+//     return ret;
+// }
+// 
+// IndexMap IndexMap::attachbefore( int to ) const 
+// {
+//     check();
+//     assert( dest.contains(to) );
+//     IndexMap ret( *this );
+//     ret.src = IndexRange( src.min() - 1, src.max() );
+//     ret.values.insert( ret.values.begin(), to );
+//     ret.check();
+//     return ret;
+// }
+
+
---
+++ legacy/indexmap_stl.hpp
@@ -0,0 +1,250 @@
+#ifndef INCLUDEGUARD_COMBINATORICS_INDEXMAP
+#define INCLUDEGUARD_COMBINATORICS_INDEXMAP
+
+
+#include <vector>
+#include <limits>
+#include <iostream>
+
+#include "../basic.hpp"
+
+#include "indexrange.hpp"
+
+
+/*****
+**
+**  This class models a mapping of indices.
+**  It has a 'source' and 'target' range.
+**  The mapping may be empty.
+**
+******/
+
+
+
+class IndexMap
+{
+    
+    public:
+        
+//         explicit IndexMap( const IndexRange& );
+        IndexMap( const IndexRange&, const std::vector<int>& );
+        IndexMap( const IndexRange&, const std::function<int(int)>& );
+        IndexMap( const IndexRange&, const std::initializer_list<int>& );
+        
+//         IndexMap( const IndexRange&, const IndexRange& );
+        IndexMap( const IndexRange&, const IndexRange&, const std::vector<int>& );
+        IndexMap( const IndexRange&, const IndexRange&, const std::function<int(int)>& );
+        IndexMap( const IndexRange&, const IndexRange&, const std::initializer_list<int>& );
+        
+        IndexMap()                              = delete;
+        IndexMap( const IndexMap& )             = default;
+        IndexMap& operator =( const IndexMap& ) = default;
+        IndexMap( IndexMap&& )                  = default;
+        IndexMap& operator =( IndexMap&& )      = default;
+        virtual ~IndexMap()                     = default; // dtor virtualization is a bit shady here
+        
+        void check() const;
+        
+        void print( std::ostream&, bool embellish = true ) const;
+        
+        const IndexRange& getSourceRange() const;
+        
+        const IndexRange& getDestRange() const;
+        
+        
+        bool isempty() const;
+        
+        bool isinjective() const;
+        
+        bool issurjective() const;
+        
+        bool isbijective() const;
+        
+        bool isstrictlyascending() const;
+        
+        
+        // TODO
+        // This interface looks like a std::vector 
+        // but it should really be a mapping.
+        // What's more, the return of references 
+        // breaks the binding.
+        
+        int& at( int i );
+        
+        const int& at( int i ) const;
+        
+        int& operator[]( int i );
+        
+        const int& operator[]( int i ) const;
+        
+        const std::vector<int>& getvalues() const;
+                
+        bool rangecontains( int value ) const;
+        
+        int preimageof( int value ) const;
+        
+        
+        
+        bool comparablewith( const IndexMap& ) const;
+        
+        bool equals( const IndexMap& ) const;
+        
+        bool less( const IndexMap& ) const;
+    
+    private:
+        
+        IndexRange src;
+        IndexRange dest;
+        
+        std::vector<int> values;
+        
+};
+
+
+
+inline IndexMap operator*( const IndexMap& leave, const IndexMap& enter )
+{
+    leave.check();
+    enter.check();
+    IndexRange src  = enter.getSourceRange();
+    IndexRange dest = leave.getDestRange();
+
+    assert( enter.getDestRange() == leave.getSourceRange() );
+
+    IndexMap ret( src, dest, [ &leave, &enter ]( int i ) -> int { return leave[ enter[i] ]; } );
+
+//     for( int i = src.min(); i <= src.max(); i++ )
+//         ret[i] = leave[ enter[i] ];
+
+    ret.check();
+    return ret;
+}
+
+inline bool operator==( const IndexMap& left, const IndexMap& right )
+{
+    left.check();
+    right.check();
+    assert( left.comparablewith( right ) );
+
+    return left.equals( right );
+}
+
+inline bool operator!=( const IndexMap& left, const IndexMap& right )
+{
+    left.check();
+    right.check();
+    assert( left.comparablewith( right ) );
+
+    return !( left.equals( right ) );
+}
+
+inline bool operator<( const IndexMap& left, const IndexMap& right )
+{
+    left.check();
+    right.check();
+    assert( left.comparablewith( right ) );
+
+    return left.less( right );
+}
+
+
+inline std::ostream& operator<<( std::ostream& os, const IndexMap& im )
+{
+    im.check();
+
+    im.print( os );
+
+    return os;
+}
+
+
+
+inline IndexMap identityIndexMap( const IndexRange& ir )
+{
+    ir.check();
+
+    IndexMap im( ir, ir, []( int i ) -> int { return i; } );
+    im.check();
+
+    return im;
+}
+
+inline IndexMap identityIndexMap( int low, int high )
+{
+    return identityIndexMap( IndexRange( low, high ) );
+}
+
+
+
+inline IndexMap expand_zero( const IndexMap& im, int p )
+{
+    const auto& src_range = im.getSourceRange();
+    const auto& dst_range = im.getDestRange();
+    
+    assert( not dst_range.isempty() );
+    assert( dst_range.min() == 0    );
+    
+    if( src_range.isempty() ) {
+        
+        return IndexMap( IndexRange(0,0), dst_range, {p} );
+        
+    } else {
+        
+        assert( src_range.min() == 0 );
+        auto new_values = im.getvalues();
+        new_values.push_back( p );
+        std::sort( new_values.begin(), new_values.end() );
+        const auto ret = IndexMap( IndexRange(0,src_range.max()+1), dst_range, new_values );
+        assert( ret.isstrictlyascending() );
+        return ret;
+        
+    }
+}
+
+
+inline IndexMap expand_one( const IndexMap& im, int p )
+{
+    const auto& src_range = im.getSourceRange();
+    const auto& dst_range = im.getDestRange();
+    
+    assert( not dst_range.isempty() );
+    assert( dst_range.min() == 0    );
+    
+    if( src_range.isempty() ) {
+        
+        return IndexMap( IndexRange(1,1), dst_range, {p} );
+        
+    } else {
+        
+        assert( src_range.min() == 1 );
+        auto new_values = im.getvalues();
+        new_values.push_back( p );
+        std::sort( new_values.begin(), new_values.end() );
+        const auto ret = IndexMap( IndexRange(1,src_range.max()+1), dst_range, new_values );
+        assert( ret.isstrictlyascending() );
+        return ret;
+        
+    }
+}
+
+
+
+
+
+// inline int fehlstelle( const IndexMap& sub, const IndexMap& super )
+// {
+//     sub.check();
+//     super.check();
+//     assert( sub.getSourceRange().getlength() == super.getSourceRange().getlength() + 1 );
+//     for( int i : sub.getSourceRange() )
+//         assert( super.rangecontains(i) );
+//     for( int j : super.getSourceRange() )
+//         if( ! sub.rangecontains(j) )
+//             return j;
+//         else 
+//             unreachable();
+// }
+
+
+
+#endif
