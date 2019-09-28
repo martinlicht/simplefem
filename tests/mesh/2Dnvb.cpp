247e995af464be2c7f97e4bd12d544597ebe4954
Martin Licht <mlicht@ucsd.edu>
2019-09-28 22:46:16 +0100

    update tests for bisection methods

--- tests/mesh/2Dnvb.cpp
+++ tests/mesh/2Dnvb.cpp
@@ -21,23 +21,37 @@ int main()
         
         {
             
+            cout << "First Experiment" << endl;
+            
             MeshSimplicial2D M = UnitTriangle2D();
             
             M.check();
             
-            for( int c = 0; c < 4; c++ ) {
+//             for( int k = 0; k < 2; k++ ) M.uniformrefinement();
             
-                std::vector<int> refinementedges;
+            int cell_count_initial = M.count_triangles();
+            int cell_marked_count  = 0;
+            
+            int c_max = 50;
+            
+            for( int c = 0; c < c_max; c++ ) {
+            
+                std::vector<int> markedcells;
                 
-                for( int k = 0; k < 3 + M.count_edges() / 10; k++ )
-                    refinementedges.push_back( rand() % M.count_edges() );
+                unsigned int p = 40;
+                for( int t = 0; t < M.count_triangles(); t++ )
+                    if( rand() % p == 0 ) 
+                        markedcells.push_back( t );
+                markedcells.push_back( 0 );
+                cell_marked_count += markedcells.size();
                 
-                std::sort( refinementedges.begin(), refinementedges.end() );
-                auto last = std::unique( refinementedges.begin(), refinementedges.end() );
-                refinementedges.erase( last, refinementedges.end() );
+                std::vector<int> markededges;
+                for( int t : markedcells ) markededges.push_back( M.get_oldest_edge( t ) );
+                sort_and_unique( markededges );
                 
-                std::cout << "Refine " << refinementedges.size() << "/" << M.count_edges() << "\n";
-                M.newest_vertex_bisection_recursive( refinementedges );
+                std::cout << c << "/" << c_max << " Refine " << markedcells.size() << "/" << M.count_triangles() << " ... ";
+                M.longest_edge_bisection_recursive( markededges );
+                std::cout << "Ratio=" << ( M.count_triangles() - cell_count_initial )/(Float)( cell_marked_count ) << nl;
             
             }
             
@@ -48,23 +62,37 @@ int main()
         
         {
             
+            cout << "Second Experiment" << endl;
+            
             MeshSimplicial2D M = StandardSquare2D();
             
             M.check();
             
-            for( int c = 0; c < 4; c++ ) {
+//             for( int k = 0; k < 2; k++ ) M.uniformrefinement();
             
-                std::vector<int> refinementedges;
+            int cell_count_initial = M.count_triangles();
+            int cell_marked_count  = 0;
+            
+            int c_max = 30;
+            
+            for( int c = 0; c < c_max; c++ ) {
+            
+                std::vector<int> markedcells;
                 
-                for( int k = 0; k < 3 + M.count_edges() / 10; k++ )
-                    refinementedges.push_back( rand() % M.count_edges() );
+                unsigned int p = 10;
+                for( int t = 0; t < M.count_triangles(); t++ )
+                    if( rand() % p == 0 ) 
+                        markedcells.push_back( t );
+                markedcells.push_back( 0 );
+                cell_marked_count += markedcells.size();
                 
-                std::sort( refinementedges.begin(), refinementedges.end() );
-                auto last = std::unique( refinementedges.begin(), refinementedges.end() );
-                refinementedges.erase( last, refinementedges.end() );
+                std::vector<int> markededges;
+                for( int t : markedcells ) markededges.push_back( M.get_oldest_edge( t ) );
+                sort_and_unique( markededges );
                 
-                std::cout << "Refine " << refinementedges.size() << "/" << M.count_edges() << "\n";
-                M.newest_vertex_bisection_recursive( refinementedges );
+                std::cout << c << "/" << c_max << "Refine " << markedcells.size() << "/" << M.count_triangles() << " ... ";
+                M.longest_edge_bisection_recursive( markededges );
+                std::cout << "Ratio=" << ( M.count_triangles() - cell_count_initial )/(Float)( cell_marked_count ) << nl;
             
             }
             
--- tests/mesh/3Dleb.cpp
+++ tests/mesh/3Dleb.cpp
@@ -21,57 +21,42 @@ int main()
         
         {
             
+            cout << "Second Experiment" << endl;
+            
             MeshSimplicial3D M = UnitSimplex3D();
             
             M.check();
             
-            for( int c = 0; c < 6; c++ ) {
+            for( int k = 0; k < 2; k++ ) M.uniformrefinement();
             
-                std::vector<int> refinementedges;
+            int cell_count_initial = M.count_tetrahedra();
+            int cell_marked_count  = 0;
+            
+            int c_max = 6;
+            
+            for( int c = 0; c < c_max; c++ ) {
+            
+                std::vector<int> markedcells;
                 
-                for( int k = 0; k < 3 + M.count_edges() / 10; k++ )
-                    refinementedges.push_back( rand() % M.count_edges() );
+                unsigned int p = 3;
+                for( int t = 0; t < M.count_tetrahedra(); t++ )
+                    if( rand() % p == 0 ) 
+                        markedcells.push_back( t );
+                cell_marked_count += markedcells.size();
                 
-                std::sort( refinementedges.begin(), refinementedges.end() );
-                auto last = std::unique( refinementedges.begin(), refinementedges.end() );
-                refinementedges.erase( last, refinementedges.end() );
+                std::vector<int> markededges;
+                for( int t : markedcells ) markededges.push_back( M.get_oldest_edge( t ) );
+                sort_and_unique( markededges );
                 
-                std::cout << "Refine " << refinementedges.size() << "/" << M.count_edges() << "\n";
-                M.longest_edge_bisection_recursive( refinementedges );
+                std::cout << c << "/" << c_max << "Refine " << markedcells.size() << "/" << M.count_tetrahedra() << " ... ";
+                M.longest_edge_bisection_recursive( markededges );
+                std::cout << "Ratio=" << ( M.count_tetrahedra() - cell_count_initial )/(Float)( cell_marked_count ) << nl;
             
             }
             
             M.check();
             
         }
-        
-        
-        {
-            
-            MeshSimplicial3D M = StandardCube3D();
-            
-            M.check();
-            
-            for( int c = 0; c < 5; c++ ) {
-            
-                std::vector<int> refinementedges;
-                
-                for( int k = 0; k < 3 + M.count_edges() / 10; k++ )
-                    refinementedges.push_back( rand() % M.count_edges() );
-                
-                std::sort( refinementedges.begin(), refinementedges.end() );
-                auto last = std::unique( refinementedges.begin(), refinementedges.end() );
-                refinementedges.erase( last, refinementedges.end() );
-                
-                std::cout << "Refine " << refinementedges.size() << "/" << M.count_edges() << "\n";
-                M.longest_edge_bisection_recursive( refinementedges );
-            
-            }
-            
-            M.check();
-            
-        }
-        
         
         cout << "Finished Unit Test" << endl;
         
