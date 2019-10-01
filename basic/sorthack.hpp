6efde228bf7413c64a574a2b1a99b223ba464bbc
Martin Licht <mlicht@ucsd.edu>
2019-10-01 01:16:29 +0100

    minor improvement of sorthack

--- basic/sorthack.hpp
+++ basic/sorthack.hpp
@@ -7,16 +7,20 @@
 template<typename A>
 void sorthack( std::vector<A>& vec )
 {
-    for( int t = 0; t < vec.size(); t++ )
+//     for( int s = 0; s < vec.size(); s++ )
+//     for( int t = 0; t < vec.size(); t++ )
+//     {
+//         if( s < t && vec[s] > vec[t] )
+//         {
+//             A temp = vec[t];
+//             vec[t] = vec[s];
+//             vec[s] = temp;
+//         }
+//     }
+    
     for( int s = 0; s < vec.size(); s++ )
-    {
-        if( t < s && vec[t] > vec[s] )
-        {
-            A temp = vec[t];
-            vec[t] = vec[s];
-            vec[s] = temp;
-        }
-    }
+    for( int t = s; t > 0 && vec.at(t-1) > vec.at(t); t-- )
+        std::swap( vec[t-1], vec[t] );
     
     for( int t = 1; t < vec.size(); t++ )
         assert( vec[t-1] <= vec[t] );
