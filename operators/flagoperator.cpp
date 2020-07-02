ca6a1f9362cc394db431dfef8b41decabf646803
Martin Licht <mlicht@ucsd.edu>
2020-07-01 22:36:48 -0700

    add flag operator class

---
+++ operators/flagoperator.cpp
@@ -0,0 +1,91 @@
+
+#include "flagoperator.hpp"
+
+#include <iostream>
+
+#include "../basic.hpp"
+#include "floatvector.hpp"
+#include "linearoperator.hpp"
+
+
+
+
+
+
+
+FlagOperator::FlagOperator( const LinearOperator& op, const std::vector<bool> destflag, const std::vector<bool> srcflag )
+: LinearOperator( op.getdimout() , op.getdimin() ), 
+  op( op ),
+  destflag( destflag ),
+  srcflag( srcflag )
+{
+    FlagOperator::check();
+}
+
+FlagOperator::FlagOperator( const LinearOperator& op, const std::vector<bool> flag )
+: FlagOperator( op, flag, flag )
+{
+    FlagOperator::check();
+}
+
+FlagOperator::~FlagOperator()
+{
+        /* Nothing */ 
+}
+
+void FlagOperator::check() const  
+{
+    LinearOperator::check();    
+    assert( getdimin()  == srcflag.size()  );
+    assert( getdimout() == destflag.size() );
+}
+
+void FlagOperator::print( std::ostream& os ) const  
+{
+    os << "Print Flag Operator" << std::endl;
+//     os << "Print Flag Operator with flag entries: " 
+//         << srcflag << '\n' << destflag << std::endl;
+}
+
+
+
+std::vector<bool>& FlagOperator::getsrcflag()
+{
+    return srcflag;
+}
+
+const std::vector<bool>& FlagOperator::getsrcflag() const
+{
+    return srcflag;
+}
+
+std::vector<bool>& FlagOperator::getdestflag()
+{
+    return destflag;
+}
+
+const std::vector<bool>& FlagOperator::getdestflag() const
+{
+    return destflag;
+}
+
+
+
+
+void FlagOperator::apply( FloatVector& dest, const FloatVector& src, Float scaling ) const 
+{
+    check();
+    src.check();
+    dest.check();
+    
+    assert( getdimin() == src.getdimension() );
+    assert( getdimout() == dest.getdimension() );
+    
+    FloatVector cloned_src = src;
+    cloned_src.clear_if( srcflag );
+    
+    op.apply( dest, cloned_src, scaling );
+    
+    dest.clear_if( destflag );
+    
+}
---
+++ operators/flagoperator.hpp
@@ -0,0 +1,68 @@
+#ifndef INCLUDEGUARD_OPERATOR_FLAGOPERATOR
+#define INCLUDEGUARD_OPERATOR_FLAGOPERATOR
+
+#include "../basic.hpp"
+#include "linearoperator.hpp"
+
+
+
+
+
+
+/************************
+****
+****  Class for flagged operators 
+****  - instantiates LinearOperator
+****  
+************************/
+
+class FlagOperator:
+public LinearOperator 
+{
+
+    public:
+
+        explicit FlagOperator( const LinearOperator& op, const std::vector<bool> destflag, const std::vector<bool> srcflag );
+        explicit FlagOperator( const LinearOperator& op, const std::vector<bool> flag );
+        virtual ~FlagOperator();
+        // TODO: Instantiate move semantics 
+
+        virtual std::shared_ptr<LinearOperator> get_shared_pointer_to_clone() const& override {
+            std::shared_ptr<FlagOperator> clone = std::make_shared<FlagOperator>( *this );
+            return clone;
+        }
+        
+        virtual std::unique_ptr<LinearOperator> get_unique_pointer_to_heir() && override {
+            std::unique_ptr<FlagOperator> heir = std::make_unique<FlagOperator>( *this );
+            return heir;
+        }
+        
+
+        virtual void check() const override;
+        virtual void print( std::ostream& ) const override;
+
+        std::vector<bool>& getsrcflag();
+        const std::vector<bool>& getsrcflag() const;
+        
+        std::vector<bool>& getdestflag();
+        const std::vector<bool>& getdestflag() const;
+        
+        virtual void apply( FloatVector& dest, const FloatVector& src, Float scaling ) const override;
+
+    private:
+
+        const LinearOperator& op;
+
+        std::vector<bool> destflag;
+        std::vector<bool> srcflag;
+            
+};
+  
+  
+
+  
+  
+  
+  
+
+#endif
