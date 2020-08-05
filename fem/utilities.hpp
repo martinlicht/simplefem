123b7ec79035d1732f71092b247e904026e003b0
Martin Licht <mlicht@ucsd.edu>
2020-08-05 12:15:26 +0200

    add comments to fem/utilities.hpp

--- fem/utilities.hpp
+++ fem/utilities.hpp
@@ -36,8 +36,17 @@ inline int SullivanSpanSize( int n, int k, int r )
 
 
 
+// 
 // Generates a matrix whose columns are the barycentric coordinates
-// of the interpolation points 
+// of the interpolation points over an n dimensional simplex 
+// such that polynomials of degree r can be reconstructed.
+// 
+// The barycentric coordinates are chosen such that 
+// the points are located in the interior of the simplex.
+// (See parameter delta)
+// 
+// Size of returned matrix:
+// [n+1] x [ n+r choose r ]
 
 inline DenseMatrix InterpolationPointsBarycentricCoordinates( int n, int r )
 {
@@ -50,6 +59,8 @@ inline DenseMatrix InterpolationPointsBarycentricCoordinates( int n, int r )
     const Float delta = +0.10000000;
     
     DenseMatrix ret( n+1, multi_indices.size(), 0. );
+    
+    assert( delta > 0 );
     
     for( int i = 0; i < multi_indices.size(); i++ )
         ret.setcolumn( i, FloatVector( multi_indices[i].getvalues() ).shift( delta ).scaleinverse( r + (n+1) * delta ) );
@@ -65,11 +76,18 @@ inline DenseMatrix InterpolationPointsBarycentricCoordinates( int n, int r )
 
 
 
-// Suppose that the columns of lpsbc are the interpolation points
-// in barycentric coordinates.
 // 
-// we generate a matrix where each column represent a barycentric polynomial
-// and the rows correspond to the interpolation points
+// Suppose that the columns of lpsbc are the interpolation points
+// (in barycentric coordinates) to interpolate degree r polynomials 
+// over a d simplex exactly.
+// 
+// The output matrix is as follows:
+// - rows correspond to the interpolation points 
+// - columns correspond to the multiindices (Lagrange basis)
+// - the entries are value of the corresponding polynomial at the corresponding point
+// 
+// Size of returned matrix:
+// [ n+r choose r ] x [ n+r choose r ]
 
 inline DenseMatrix EvaluationMatrix( int dim, int r, const DenseMatrix& lpsbc )
 {
@@ -77,15 +95,15 @@ inline DenseMatrix EvaluationMatrix( int dim, int r, const DenseMatrix& lpsbc )
     
     const auto mis = generateMultiIndices( IndexRange(0,dim), r );
     
-    assert( dim+1      == lpsbc.getdimout() );
-    assert( mis.size() == lpsbc.getdimin()  );
-    
+    assert( dim+1                == lpsbc.getdimout()          );
+    assert( mis.size()           == lpsbc.getdimin()           );
+    assert( multi_indices.size() == binomial_integer( n+r, r ) );
     const int N = mis.size();
     
     DenseMatrix ret( N, N );
     
     for( int col = 0; col < N; col++ ) // col -> barycentric poly 
-    for( int row = 0; row < N; row++ ) // row -> interpolation point // FIXME: shadowed variable 
+    for( int row = 0; row < N; row++ ) // row -> interpolation point 
     {
         ret(row,col) = 1.;
         for( int d = 0; d <= dim; d++ )
@@ -99,12 +117,19 @@ inline DenseMatrix EvaluationMatrix( int dim, int r, const DenseMatrix& lpsbc )
 
 
 
-// Suppose that the columns of lpsbc are the interpolation points
-// in barycentric coordinates.
 // 
-// we generate a matrix where each column represent a barycentric polynomial
-// and the rows correspond to the interpolation points
-
+// Suppose that the columns of lpsbc are the interpolation points
+// (in barycentric coordinates) to interpolate degree r polynomials 
+// over a d simplex exactly.
+// 
+// Suppose that mis cointains a collection of multiindices
+// over a d simplex 
+// 
+// The output matrix is as follows:
+// - columns correspond to the interpolation points 
+// - rows correspond to the multiindices (Lagrange basis)
+// - the entries are value of the corresponding polynomial at the corresponding point
+// 
 // TODO Reread and compare with previous version 
 
 inline DenseMatrix EvaluationMatrix( std::vector<MultiIndex> mis, const DenseMatrix& lpsbc )
@@ -158,6 +183,34 @@ inline DenseMatrix EvaluationMatrix( std::vector<MultiIndex> mis, const DenseMat
 // }
 
 
+
+
+
+
+
+
+
+
+
+
+
+// 
+// The input matrix J is a Jacobian of the transformation which transforms 
+// the [dimin] reference simplex onto a simplex 
+// within [dimout] dimensional ambient space 
+// 
+// We transpose the matrix and prepend a zero row.
+// 
+// If we multiply a vector in standard basis coordinates 
+// we get the same vector in barycentric coordinates, 
+// using only the gradients 1 through n. 
+// Gradient 0 is redundant and thus the zero-th row is 0.
+// 
+// Size of output matrix:
+// [ J.dimin()+1 x J.dimout() ]
+// 
+
+
 inline DenseMatrix BarycentricProjectionMatrix( const DenseMatrix& J )
 {
     assert( J.getdimout() >= J.getdimin() );
@@ -178,8 +231,18 @@ inline DenseMatrix BarycentricProjectionMatrix( const DenseMatrix& J )
 
 
 
+// 
 // evaluate a field at given physical points 
 // and collect its values 
+// 
+// Given a k-differential formin dim dimensional ambient space 
+// and lps being a matrix of size [outerdimension] x [var1]
+// we let the output be as follows.
+// 
+// The output is a matrix of size [dim choose k] x [var1]
+// whose column are the evaluations of the field 
+// at each of the interpolation points.
+// 
 
 inline DenseMatrix EvaluateField( 
             int dim, int k,  
@@ -233,6 +296,9 @@ inline DenseMatrix EvaluateField(
 // 
 // 3. Transform the Euclidean components to their barycentric projections
 // 
+// The output is a vector whose coefficients represent the volume-wise interpolations 
+// into the local Sullivan space of k-forms with polynomial degree r 
+// 
 
 
 inline FloatVector Interpolation( 
@@ -248,7 +314,16 @@ inline FloatVector Interpolation(
     
     FloatVector ret( m.count_simplices(dim) * SullivanSpanSize(dim,k,r) );
     
-    //const auto degree_r_mis = generateMultiIndices( IndexRange(0,dim), r );
+    
+    // lpsbc are the barycentric coordinates to interpolate 
+    // degree r polynomials over a dim simplex 
+    // [dim+1] x [number of lagrange points] 
+    // 
+    // EM is the values of the Lagrange polynomials at those points 
+    // [n+r choose r] x [number of lagrange points] (actually, square)
+    // 
+    // EMinv is the inverse of EM.
+    // [same size as EM]
     
     const auto lpsbc = InterpolationPointsBarycentricCoordinates( dim, r );
     
@@ -269,37 +344,63 @@ inline FloatVector Interpolation(
     for( int s = 0; s < m.count_simplices(dim); s++ )
     {
         
+        // We obtain the coordinate matrix (outerdim) x (dim+1) 
+        // of the dim-dimensional simplex with index s.
+        // Each column contains the physical coordinates of the vertices
+        // 
+        // lps are the physical coordinates of the interpolation points 
+        // (outerdim) x (number of Lagrange points)
+        // 
+        
         const auto coords = m.getVertexCoordinateMatrix( dim, s );
         
         const auto lps = coords * lpsbc;
         
-//         LOG << lps << space << lpsbc << coords << nl;
-        
-        assert( lps.isfinite() );
+        // 
+        // Jac is a matrix of size [outerdimension] x [dim]
+        // which is the jacobian of the transformation mapping of simplex s
+        // 
+        // bpm is a matrix of size [dim+1]x[outerdimension]
+        // that is the barycentric projection matrix (see paper)
+        // 
+        // P is the matrix of subdeterminants of size k 
+        // of that matrix 
+        // 
         
         const auto Jac = m.getTransformationJacobian( dim, s );
         
         const auto bpm = BarycentricProjectionMatrix( Jac );
         
-        const auto P = SubdeterminantMatrix( bpm, k ); //MatrixAlternatingPower( bpm, k );
+        const auto P = SubdeterminantMatrix( bpm, k );
         
-        assert( P.isfinite()    );
+        // The InterpolationMatrix is the tensor product matrix 
+        // of EMinv and P.
+        // 
+        // Evaluations is a matrix of size [dim choose k] x [lps.dimin]
+        // whose columsn are the outputs of the field at the Lagrange points
+        // 
+        // localResults contains the coeffecients of the interpolation
+        // in the canonical basis (Lagrange polynomials x barycentric gradients)
         
         const auto InterpolationMatrix = MatrixTensorProduct( EMinv, P );
-        assert( InterpolationMatrix.isfinite()    );
         
-        const auto Evaluations = EvaluateField( dim, k, lps, field );
+        const auto Evaluations      = EvaluateField( dim, k, lps, field );
         const auto EvaluationVector = Evaluations.flattencolumns();
-        
-        assert( Evaluations.isfinite()      );
-        assert( EvaluationVector.isfinite() );
-        
-//         LOG << InterpolationMatrix.getdimin() << space << EvaluationVector.getdimension() << nl;
         
         const auto localResult = InterpolationMatrix * EvaluationVector;
 
+        #ifndef NDEBUG
+
+        assert( lps.isfinite() );
+        
+        assert( P.isfinite()    );
+        
+        assert( InterpolationMatrix.isfinite()    );
+
+        assert( Evaluations.isfinite()      );
+        assert( EvaluationVector.isfinite() );
+        
         assert( localResult.isfinite()    );
-        // LOG << EvaluationVector << std::endl;
 
         if( k == 0 ) {
             if( !( EM * localResult - EvaluationVector ).issmall() ) {
@@ -310,6 +411,7 @@ inline FloatVector Interpolation(
             assert( ( EM * localResult - EvaluationVector ).issmall() );
             assert( ( InterpolationMatrix - EMinv ).issmall() );
         }
+        #endif
         
         assert( localResult.getdimension() == SullivanSpanSize(dim,k,r) );
         
