a8ecfb55360e1c62ee760774ded3b2b21b31cfa9
Martin Licht <mlicht@ucsd.edu>
2020-08-19 18:22:14 +0200

    additional variants of the classical sparse CGM solver

---
+++ tests/experiments/solvercomp2.cpp
@@ -0,0 +1,321 @@
+
+
+/**/
+
+#include <iostream>
+#include <fstream>
+#include <iomanip>
+
+#include "../../basic.hpp"
+#include "../../utility/utility.hpp"
+#include "../../operators/composedoperators.hpp"
+// #include "../../operators/composed.hpp"
+#include "../../dense/densematrix.hpp"
+#include "../../sparse/sparsematrix.hpp"
+#include "../../sparse/matcsr.hpp"
+#include "../../mesh/coordinates.hpp"
+#include "../../mesh/mesh.simplicial2D.hpp"
+#include "../../mesh/examples2D.hpp"
+#include "../../vtk/vtkwriter.mesh2D.hpp"
+#include "../../solver/sparsesolver.hpp"
+#include "../../solver/cgm.hpp"
+#include "../../solver/crm.hpp"
+// #include "../../solver/pcrm.hpp"
+#include "../../solver/minres.hpp"
+#include "../../fem/local.polynomialmassmatrix.hpp"
+#include "../../fem/global.massmatrix.hpp"
+#include "../../fem/global.diffmatrix.hpp"
+#include "../../fem/global.lagrangeincl.hpp"
+#include "../../fem/utilities.hpp"
+
+
+using namespace std;
+
+int main()
+{
+        
+        cout << "Unit Test: Compare numerical solvers CRM vs MINRES\n           for Solution of Dirichlet Problem" << endl;
+        
+        cout << std::setprecision(10);
+
+        if(true){
+
+            cout << "Initial mesh..." << endl;
+            
+            MeshSimplicial2D M = UnitSquare2D();
+            
+            M.check();
+            
+            M.automatic_dirichlet_flags();
+            
+            M.check_dirichlet_flags();
+            
+            cout << "Prepare scalar fields for testing..." << endl;
+            
+
+            std::function<FloatVector(const FloatVector&)> constant_one
+                = [](const FloatVector& vec) -> FloatVector{
+                        assert( vec.getdimension() == 2 );
+                        return FloatVector({ 1. });
+                    };
+            
+            Float xfeq = 1.;
+            Float yfeq = 1.;
+            
+            std::function<FloatVector(const FloatVector&)> experiment_sol = 
+                [=](const FloatVector& vec) -> FloatVector{
+                    assert( vec.getdimension() == 2 );
+                    return FloatVector({ std::sin( xfeq * Constants::twopi * vec[0] ) * std::sin( yfeq * Constants::twopi * vec[1] ) });
+                };
+            
+
+            std::function<FloatVector(const FloatVector&)> experiment_grad = 
+                [=](const FloatVector& vec) -> FloatVector{
+                    assert( vec.getdimension() == 2 );
+                    return FloatVector( { 
+                            xfeq * Constants::twopi * std::cos( xfeq * Constants::twopi * vec[0] ) * std::sin( yfeq * Constants::twopi * vec[1] ),
+                            yfeq * Constants::twopi * std::sin( xfeq * Constants::twopi * vec[0] ) * std::cos( yfeq * Constants::twopi * vec[1] ), 
+                        });
+                };
+            
+
+            std::function<FloatVector(const FloatVector&)> experiment_rhs = 
+                [=](const FloatVector& vec) -> FloatVector{
+                    assert( vec.getdimension() == 2 );
+                    return FloatVector({ 
+                        
+                        xfeq*xfeq * Constants::fourpisquare * std::sin( xfeq * Constants::twopi * vec[0] ) * std::sin( yfeq * Constants::twopi * vec[1] )
+                        +
+                        yfeq*yfeq * Constants::fourpisquare * std::sin( xfeq * Constants::twopi * vec[0] ) * std::sin( yfeq * Constants::twopi * vec[1] )
+                     });
+                };
+            
+
+            
+            
+            
+
+            cout << "Solving Poisson Problem with Dirichlet boundary conditions" << endl;
+
+            ConvergenceTable contable;
+            
+
+            int min_l = 3; int max_l = 9;
+
+            for( int l = 0; l < min_l; l++ )
+                M.uniformrefinement();
+
+            for( int l = min_l; l <= max_l; l++ ){
+                
+                cout << "Level: " << l << std::endl;
+                cout << "# T/E/V: " << M.count_triangles() << "/" << M.count_edges() << "/" << M.count_vertices() << nl;
+                
+                const int r = 1;
+                
+                {
+                    
+                    cout << "...assemble matrices" << endl;
+            
+                    SparseMatrix scalar_massmatrix = FEECBrokenMassMatrix( M, M.getinnerdimension(), 0, r );
+                                        
+                    cout << "...assemble differential matrix and transpose" << endl;
+
+                    SparseMatrix diffmatrix = FEECBrokenDiffMatrix( M, M.getinnerdimension(), 0, r );
+
+                    SparseMatrix diffmatrix_t = diffmatrix.getTranspose();
+
+                    cout << "...assemble inclusion matrix and transpose" << endl;
+            
+                    SparseMatrix incmatrix = LagrangeInclusionMatrix( M, M.getinnerdimension(), r );
+
+                    SparseMatrix incmatrix_t = incmatrix.getTranspose();
+
+                    cout << "...assemble stiffness matrix" << endl;
+            
+                    // ProductOperator 
+//                     auto stiffness = incmatrix_t * diffmatrix_t * vector_massmatrix * diffmatrix * incmatrix;
+                       
+//                     auto op1 = incmatrix_t * diffmatrix_t;
+//                     auto op2 = op1 * vector_massmatrix;
+//                     auto op3 = op2 * diffmatrix;
+//                     auto stiffness = op3 * incmatrix;
+
+//                     auto opr1 = diffmatrix & incmatrix;
+//                     auto opr  = vector_massmatrix_fac & opr1;
+//                     auto opl  = opr.getTranspose(); 
+//                     auto stiffness = opl & opr;
+
+                    auto opr  = diffmatrix & incmatrix;
+                    auto opl  = opr.getTranspose(); 
+                    auto stiffness_prelim = opl & opr;
+                    stiffness_prelim.sortentries();
+                    auto stiffness = MatrixCSR( stiffness_prelim );
+                    
+                    {
+
+                        FloatVector sol( M.count_simplices(0), 0. );
+                        
+                        const auto& function_rhs  = experiment_rhs;
+                        FloatVector interpol_rhs  = Interpolation( M, M.getinnerdimension(), 0, r,   function_rhs  );
+                        FloatVector rhs = incmatrix_t * ( scalar_massmatrix * interpol_rhs );
+
+                        {
+                            cout << "CGM - CSR Classic" << endl;
+                        
+                            sol.zero();
+                            timestamp start = gettimestamp();
+                            FloatVector residual( rhs );
+                            ConjugateGradientSolverCSR( 
+                                sol.getdimension(), 
+                                sol.raw(), 
+                                rhs.raw(), 
+                                stiffness.getA(), stiffness.getC(), stiffness.getV(),
+                                residual.raw(),
+                                1e-16,
+                                1
+                            );
+
+                            timestamp end = gettimestamp();
+                            std::cout << "\t\t\t Time: " << timestamp2string( end - start ) << std::endl;
+                            contable << Float(end - start) << Float(1.);
+                        }
+
+                        {
+                            cout << "CGM - CSR Variant" << endl;
+                        
+                            sol.zero();
+                            timestamp start = gettimestamp();
+                            FloatVector residual( rhs );
+                            ConjugateGradientSolverCSR_variant( 
+                                sol.getdimension(), 
+                                sol.raw(), 
+                                rhs.raw(), 
+                                stiffness.getA(), stiffness.getC(), stiffness.getV(),
+                                residual.raw(),
+                                1e-16,
+                                1
+                            );
+
+                            timestamp end = gettimestamp();
+                            std::cout << "\t\t\t Time: " << timestamp2string( end - start ) << std::endl;
+                            contable << Float(end - start) << Float(1.);
+                        }
+
+                        {
+                            cout << "CGM - CSR SStep" << endl;
+                        
+                            sol.zero();
+                            timestamp start = gettimestamp();
+                            FloatVector residual( rhs );
+                            ConjugateGradientSolverCSR_sstep( 
+                                sol.getdimension(), 
+                                sol.raw(), 
+                                rhs.raw(), 
+                                stiffness.getA(), stiffness.getC(), stiffness.getV(),
+                                residual.raw(),
+                                1e-16,
+                                1
+                            );
+
+                            timestamp end = gettimestamp();
+                            std::cout << "\t\t\t Time: " << timestamp2string( end - start ) << std::endl;
+                            contable << Float(end - start) << Float(1.);
+                        }
+
+                        {
+                            cout << "CRM - CSR Old" << endl;
+                        
+                            sol.zero();
+                            timestamp start = gettimestamp();
+                            FloatVector residual( rhs );
+                            ConjugateResidualSolverCSR_old( 
+                                sol.getdimension(), 
+                                sol.raw(), 
+                                rhs.raw(), 
+                                stiffness.getA(), stiffness.getC(), stiffness.getV(),
+                                residual.raw(),
+                                1e-16,
+                                1
+                            );
+
+                            timestamp end = gettimestamp();
+                            std::cout << "\t\t\t Time: " << timestamp2string( end - start ) << std::endl;
+                            contable << Float(end - start) << Float(1.);
+                        }
+
+                        {
+                            cout << "CRM - CSR Sstep" << endl;
+                        
+                            sol.zero();
+                            timestamp start = gettimestamp();
+                            FloatVector residual( rhs );
+                            ConjugateResidualSolverCSR( 
+                                sol.getdimension(), 
+                                sol.raw(), 
+                                rhs.raw(), 
+                                stiffness.getA(), stiffness.getC(), stiffness.getV(),
+                                residual.raw(),
+                                1e-16,
+                                1
+                            );
+
+                            timestamp end = gettimestamp();
+                            std::cout << "\t\t\t Time: " << timestamp2string( end - start ) << std::endl;
+                            contable << Float(end - start) << Float(1.);
+                        }
+
+                        if(false){
+                            cout << "CGM" << endl;
+                        
+                            sol.zero();
+                            ConjugateGradientMethod Solver( stiffness );
+                            Solver.print_modulo        = 1 + 4 * sol.getdimension();
+                            Solver.max_iteration_count =     4 * sol.getdimension();
+                            timestamp start = gettimestamp();
+                            Solver.solve( sol, rhs );
+//                             Solver.solve( sol, rhs );
+                            timestamp end = gettimestamp();
+                            std::cout << "\t\t\t Time: " << timestamp2string( end - start ) << std::endl;
+                            
+                            contable << Float(end - start) << Float(Solver.recent_iteration_count);
+                        }
+
+                        if(false){
+                            cout << "MINRES" << endl;
+                        
+                            sol.zero();
+                            MinimumResidualMethod Solver( stiffness );
+                            Solver.print_modulo        = 1; + 4 * sol.getdimension();
+                            Solver.max_iteration_count =     4 * sol.getdimension();
+                            timestamp start = gettimestamp();
+                            Solver.solve( sol, rhs );
+                            timestamp end = gettimestamp();
+                            std::cout << "\t\t\t Time: " << timestamp2string( end - start ) << std::endl;
+
+                            contable << Float(end - start) << Float(Solver.recent_iteration_count);
+                        }
+                        
+                        contable << nl;
+                        
+                        contable.print( std::cout );
+
+                    }
+                    
+                }
+
+                cout << "Refinement..." << endl;
+            
+                if( l != max_l ) M.uniformrefinement();
+
+                
+            } 
+        
+        }
+        
+        
+        
+        
+        cout << "Finished Unit Test" << endl;
+        
+        return 0;
+}
