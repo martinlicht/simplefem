6744be3bf976fbb3ad5cbaf646670dffeaa2247a
Martin Licht <martin.licht@epfl.ch>
2022-09-16 14:12:40 +0200

    working on 3D curl curl example

--- tests/sullivan3D/curlcurl3D.cpp
+++ tests/sullivan3D/curlcurl3D.cpp
@@ -81,11 +81,15 @@ int main()
         };
     
     // ( v_x - u_y ) dxy + ( w_x - u_z ) dxz + ( w_y - v_z ) dyz
+    // 
     // -> (Hodge)
+    // 
     // ( v_x - u_y ) dz  - ( w_x - u_z ) dy  + ( w_y - v_z ) dx
     // = 
     //  v_x dz - u_y dz  -  w_x dy + u_z dy  +  w_y dx - v_z dx
+    // 
     // -> (Cartan)
+    // 
     // v_xx dxz - u_xy dxz -  w_xx dxy + u_xz dxy  +  w_yy dyx - v_yz dyx
     // +
     // v_yx dyz - u_yy dyz -  w_xz dzy + u_zz dzy  +  w_zy dzx - v_zz dzx
@@ -93,38 +97,46 @@ int main()
     // v_xx dxz - u_xy dxz -  w_xx dxy + u_xz dxy  -  w_yy dxy + v_yz dxy
     // +
     // v_yx dyz - u_yy dyz +  w_xz dyz - u_zz dyz  -  w_yy dxy + v_yz dxz
+    // 
     // -> (Hodge)
+    // 
     // - v_xx dy + u_xy dy -  w_xx dz + u_xz dz  -  w_yy dz + v_yz dz
     // +
-    //   v_yx dx - u_yy dx +  w_xz dx - u_zz dx  +  w_yy dy - v_yz dy
+    // + v_yx dx - u_yy dx +  w_xz dx - u_zz dx  +  w_yy dy - v_yz dy
     // = 
-    // 
-    //   v_yx dx - u_yy dx +  w_xz dx - u_zz dx
+    // + v_yx dx - u_yy dx +  w_xz dx - u_zz dx
     // - v_xx dy + u_xy dy +  w_yy dy - v_yz dy
     // - w_xx dz + u_xz dz -  w_yy dz + v_yz dz
-    // -> multiply by (-1)^(n(k-1)+1) = (-1)^(3+1)
+    // 
+    // -> multiply by (-1)^(n(k-1)+1) = (-1)^(3(2-1)+1) = 1
 
     std::function<FloatVector(const FloatVector&)> experiment_rhs = 
         [=](const FloatVector& vec) -> FloatVector{
             assert( vec.getdimension() == 3 );
             return FloatVector({ 
                 blob_dev(vec[0]) * blob(vec[1]) * blob(vec[2]) 
-                -
+                +
                 (
-                + blob( vec[0] ) * blob( vec[1] ) * blob_dev( vec[2] ) + blob( vec[0] ) * blob_dev( vec[1] ) * blob( vec[2] ) // u
-                + blob( vec[0] ) * blob( vec[1] ) * blob_dev( vec[2] ) + blob( vec[0] ) * blob_dev( vec[1] ) * blob( vec[2] ) // u
-                + blob( vec[0] ) * blob( vec[1] ) * blob_dev( vec[2] ) + blob( vec[0] ) * blob_dev( vec[1] ) * blob( vec[2] ) // u
-                ) 
+                + blob_devdev( vec[0] ) * blob( vec[1] ) * blob_dev( vec[2] ) + blob_devdev( vec[0] ) * blob_dev( vec[1] ) * blob( vec[2] )
+                + blob( vec[0] ) * blob_devdev( vec[1] ) * blob_dev( vec[2] ) + blob( vec[0] ) * blob_devdevdev( vec[1] ) * blob( vec[2] )
+                + blob( vec[0] ) * blob( vec[1] ) * blob_devdevdev( vec[2] ) + blob( vec[0] ) * blob_dev( vec[1] ) * blob_devdev( vec[2] )
+                )
                 ,
                 blob(vec[0]) * blob_dev(vec[1]) * blob(vec[2]) 
                 + 
-                // ( + blob_devdevdev( vec[0] ) * blob_dev( vec[1] ) + blob_dev( vec[0] ) * blob_devdev( vec[1] ) )
-                (   24. * vec[0] * square( vec[1]*vec[1] - 1. )      + ( 4. * vec[0] * ( vec[0]*vec[0] - 1. ) ) * ( 12. * vec[1]*vec[1] - 4. ) )
+                (
+                + blob_devdev( vec[0] ) * blob( vec[1] ) * blob_dev( vec[2] ) - blob_devdevdev( vec[0] ) * blob( vec[1] ) * blob( vec[2] ) // v 
+                + blob( vec[0] ) * blob_devdev( vec[1] ) * blob_dev( vec[2] ) - blob_dev( vec[0] ) * blob_devdev( vec[1] ) * blob( vec[2] ) // v 
+                + blob( vec[0] ) * blob( vec[1] ) * blob_devdevdev( vec[2] ) - blob_dev( vec[0] ) * blob( vec[1] ) * blob_devdev( vec[2] ) // v 
+                )
                 ,
                 blob(vec[0]) * blob(vec[1]) * blob_dev(vec[2])
                 + 
-                // ( + blob_devdevdev( vec[0] ) * blob_dev( vec[1] ) + blob_dev( vec[0] ) * blob_devdev( vec[1] ) )
-                (   24. * vec[0] * square( vec[1]*vec[1] - 1. )      + ( 4. * vec[0] * ( vec[0]*vec[0] - 1. ) ) * ( 12. * vec[1]*vec[1] - 4. ) )
+                (
+                - blob_devdevdev( vec[0] ) * blob( vec[1] ) * blob( vec[2] ) - blob_devdev( vec[0] ) * blob_dev( vec[1] ) * blob( vec[2] ) // w 
+                - blob_dev( vec[0] ) * blob_devdev( vec[1] ) * blob( vec[2] ) - blob( vec[0] ) * blob_devdevdev( vec[1] ) * blob( vec[2] ) // w 
+                - blob_dev( vec[0] ) * blob( vec[1] ) * blob_devdev( vec[2] ) - blob( vec[0] ) * blob_dev( vec[1] ) * blob_devdev( vec[2] ) // w 
+                )
                 });
         };
     
@@ -147,23 +159,24 @@ int main()
     LOG << "Solving Poisson Problem with Neumann boundary conditions" << nl;
 
     const int min_l = 0; 
-    const int max_l = 3;
+    const int max_l = 5;
     
     const int min_r = 1;
     const int max_r = 1;
 
-    const int r_plus_vector = 1;
-    const int r_plus_scalar = 1;
+    const int r_plus_vector = 2;
+    const int r_plus_scalar = 2;
+    const int r_plus_pseudo = 2;
     
     
     ConvergenceTable contable("Mass error and numerical residuals");
     
-    contable << "u_error" << "sigma_error" << "u_res" << "sigma_res" << "time";
+    contable << "u_error" << "du_error" << "sigma_error" << "u_res" << "sigma_res" << "time";
     
 
     assert( 0 <= min_l and min_l <= max_l );
     assert( 0 <= min_r and min_r <= max_r );
-    assert( 0 <= r_plus_scalar and 0 <= r_plus_vector);
+    assert( 0 <= r_plus_scalar and 0 <= r_plus_vector and 0 <= r_plus_pseudo );
       
     for( int l = 0; l < min_l; l++ )
         M.uniformrefinement();
@@ -181,18 +194,22 @@ int main()
                     
             LOG << "integration with: " << r_plus_vector << ", " << r_plus_scalar << nl;
                     
-            LOG << "... assemble matrices" << nl;
+            LOG << "... assemble mass matrices" << nl;
     
             SparseMatrix scalar_massmatrix = FEECBrokenMassMatrix( M, M.getinnerdimension(), 0, r+1 + r_plus_scalar );
-            SparseMatrix vector_massmatrix = FEECBrokenMassMatrix( M, M.getinnerdimension(), 1, r   + r_plus_vector);
-            SparseMatrix volume_massmatrix = FEECBrokenMassMatrix( M, M.getinnerdimension(), 2, r-1 );
+            SparseMatrix vector_massmatrix = FEECBrokenMassMatrix( M, M.getinnerdimension(), 1, r   + r_plus_vector );
+            SparseMatrix pseudo_massmatrix = FEECBrokenMassMatrix( M, M.getinnerdimension(), 2, r-1 + r_plus_pseudo );
 
+            LOG << "... assemble inclusion matrices" << nl;
+    
             SparseMatrix scalar_incmatrix   = FEECSullivanInclusionMatrix( M, M.getinnerdimension(), 0, r+1 );
             SparseMatrix scalar_incmatrix_t = scalar_incmatrix.getTranspose();
 
             SparseMatrix vector_incmatrix   = FEECSullivanInclusionMatrix( M, M.getinnerdimension(), 1, r   );
             SparseMatrix vector_incmatrix_t = vector_incmatrix.getTranspose();
 
+            LOG << "... assemble algebraic matrices" << nl;
+    
             SparseMatrix scalar_diffmatrix   = FEECBrokenDiffMatrix( M, M.getinnerdimension(), 0, r+1 );
             SparseMatrix scalar_diffmatrix_t = scalar_diffmatrix.getTranspose();
 
@@ -205,15 +222,22 @@ int main()
             SparseMatrix vector_elevmatrix   = FEECBrokenElevationMatrix( M, M.getinnerdimension(), 1, r  , r_plus_vector );
             SparseMatrix vector_elevmatrix_t = vector_elevmatrix.getTranspose();
 
-            auto mat_A  = vector_incmatrix_t & vector_diffmatrix_t & volume_massmatrix & vector_diffmatrix & vector_incmatrix;
+            SparseMatrix pseudo_elevmatrix   = FEECBrokenElevationMatrix( M, M.getinnerdimension(), 2, r-1, r_plus_pseudo );
+            SparseMatrix pseudo_elevmatrix_t = pseudo_elevmatrix.getTranspose();
+
+            LOG << "... compose system matrices" << nl;
+    
+            auto mat_A  = vector_incmatrix_t & vector_diffmatrix_t & ( pseudo_elevmatrix_t & pseudo_massmatrix & pseudo_elevmatrix ) & vector_diffmatrix & vector_incmatrix;
             mat_A.sortandcompressentries();
                 
-            auto mat_Bt = vector_incmatrix_t & vector_elevmatrix_t & vector_massmatrix & vector_elevmatrix & scalar_diffmatrix & scalar_incmatrix; // upper right
+            auto mat_Bt = vector_incmatrix_t & ( vector_elevmatrix_t & vector_massmatrix & vector_elevmatrix ) & scalar_diffmatrix & scalar_incmatrix; // upper right
             mat_Bt.sortandcompressentries();
             
-            auto mat_B = mat_Bt.getTranspose(); //volume_incmatrix_t & volume_massmatrix & diffmatrix & vector_incmatrix; // lower bottom
+            auto mat_B = mat_Bt.getTranspose(); //volume_incmatrix_t & pseudo_massmatrix & diffmatrix & vector_incmatrix; // lower bottom
             mat_B.sortandcompressentries();
             
+            LOG << "... compose CSR system matrices" << nl;
+    
             auto A  = MatrixCSR( mat_A  );
             auto Bt = MatrixCSR( mat_Bt );
             auto B  = MatrixCSR( mat_B  );
@@ -224,22 +248,24 @@ int main()
             // auto PA = IdentityMatrix( A.getdimin() );
             // auto PC = IdentityMatrix( C.getdimin() );
             auto PA = MatrixCSR( vector_incmatrix_t & vector_elevmatrix_t & vector_massmatrix & vector_elevmatrix & vector_incmatrix )
-                              + MatrixCSR( vector_incmatrix_t & vector_diffmatrix_t & volume_massmatrix & vector_diffmatrix & vector_incmatrix );
+                              + MatrixCSR( vector_incmatrix_t & vector_diffmatrix_t & pseudo_elevmatrix_t & pseudo_massmatrix & pseudo_elevmatrix & vector_diffmatrix & vector_incmatrix );
             auto PC = MatrixCSR( scalar_incmatrix_t & scalar_diffmatrix_t & vector_elevmatrix_t & vector_massmatrix & vector_elevmatrix & scalar_diffmatrix & scalar_incmatrix );
                 
             LOG << "share zero PA = " << PA.getnumberofzeroentries() << "/" <<  PA.getnumberofentries() << nl;
             LOG << "share zero PC = " << PC.getnumberofzeroentries() << "/" <<  PC.getnumberofentries() << nl;
                         
                         
-            const auto& function_sol = experiment_sol;
-            const auto& function_rhs = experiment_rhs;
-            const auto& function_aux = experiment_aux;
+            const auto& function_sol  = experiment_sol;
+            const auto& function_rhs  = experiment_rhs;
+            const auto& function_aux  = experiment_aux;
+            const auto& function_curl = experiment_curl;
             
             LOG << "...interpolate explicit solution and rhs" << nl;
             
-            FloatVector interpol_sol = Interpolation( M, M.getinnerdimension(), 1, r   + r_plus_vector, function_sol );
-            FloatVector interpol_rhs = Interpolation( M, M.getinnerdimension(), 1, r   + r_plus_vector, function_rhs );
-            FloatVector interpol_aux = Interpolation( M, M.getinnerdimension(), 0, r+1 + r_plus_scalar, function_aux );
+            FloatVector interpol_sol  = Interpolation( M, M.getinnerdimension(), 1, r   + r_plus_vector, function_sol  );
+            FloatVector interpol_rhs  = Interpolation( M, M.getinnerdimension(), 1, r   + r_plus_vector, function_rhs  );
+            FloatVector interpol_aux  = Interpolation( M, M.getinnerdimension(), 0, r+1 + r_plus_scalar, function_aux  );
+            FloatVector interpol_curl = Interpolation( M, M.getinnerdimension(), 2, r-1 + r_plus_vector, function_curl );
             
             FloatVector rhs_sol = vector_incmatrix_t * vector_elevmatrix_t * vector_massmatrix * interpol_rhs;
             FloatVector rhs_aux = scalar_incmatrix_t * scalar_diffmatrix_t * vector_elevmatrix_t * vector_massmatrix * interpol_sol ;// FloatVector( B.getdimout(), 0. );
@@ -276,11 +302,13 @@ int main()
             
             LOG << "...compute error and residual:" << nl;
 
-            auto errornorm_aux_sol = interpol_sol - vector_elevmatrix * vector_incmatrix * sol;
-            auto errornorm_aux_aux = interpol_aux - scalar_elevmatrix * scalar_incmatrix * aux;
+            auto errornorm_aux_sol  = interpol_sol  - vector_elevmatrix * vector_incmatrix * sol;
+            auto errornorm_aux_curl = interpol_curl - pseudo_elevmatrix * vector_diffmatrix * vector_incmatrix * sol;
+            auto errornorm_aux_aux  = interpol_aux  - scalar_elevmatrix * scalar_incmatrix * aux;
 
-            Float errornorm_sol = sqrt( errornorm_aux_sol * ( vector_massmatrix * errornorm_aux_sol ) );
-            Float errornorm_aux = sqrt( errornorm_aux_aux * ( scalar_massmatrix * errornorm_aux_aux ) );
+            Float errornorm_sol  = sqrt( errornorm_aux_sol  * ( vector_massmatrix * errornorm_aux_sol  ) );
+            Float errornorm_curl = sqrt( errornorm_aux_curl * ( pseudo_massmatrix * errornorm_aux_curl ) );
+            Float errornorm_aux  = sqrt( errornorm_aux_aux  * ( scalar_massmatrix * errornorm_aux_aux  ) );
             
             Float residual_sol  = ( rhs_sol - A * sol - Bt * aux ).norm();
             Float residual_aux  = ( rhs_aux - B * sol -  C * aux ).norm();
@@ -293,6 +321,7 @@ int main()
             LOG << "aux rhs: " << rhs_aux.norm() << nl;
 
             contable << errornorm_sol;
+            contable << errornorm_curl;
             contable << errornorm_aux;
             contable << residual_sol;
             contable << residual_aux;
