4047e3c2ea41c565aca5306b88240a8bf03d8e59
Martin Licht <contact@martinwernerlicht.de>
2023-04-07 10:58:27 +0200

    improve image output

--- utility/images.cpp
+++ utility/images.cpp
@@ -61,8 +61,35 @@ using namespace Magick;
 
 
 /*
-    <polygon points="10,20 40,50 0010,10" fill="#FF3399" stroke="none"></polygon>
+    <polygon points="10,20 30,40 50,60" fill="#FF3399" stroke="none"></polygon>
     <polygon points="10,20 40,50 0010,10" fill="#FF3399" stroke="#11CCDD" stroke-width="1"></polygon>
+
+    #include <iostream>
+    #include <sstream>
+    #include <string>
+
+    std::string generatePolygonString(
+        double x1, double y1, double x2, double y2, double x3, double y3, 
+        std::string fill, 
+        std::string stroke
+    ) {
+        std::stringstream ss;
+        ss << "<polygon points=\"" 
+           << x1 << "," << y1 << " " 
+           << x2 << "," << y2 << " " 
+           << x3 << "," << y3 << " "  
+           << "\" fill=\"" << fill 
+           << "\" stroke=\"" << stroke
+           << "\"></polygon>";
+        return ss.str();
+    }
+
+    int main() {
+        std::string polygonString = generatePolygonString(10, 20, 30, 40, 50, 60, "#FF3399", "none");
+        std::cout << polygonString << std::endl;
+        return 0;
+    }
+
 
     void writeSVG( 
         const std::string filename,
@@ -86,19 +113,19 @@ using namespace Magick;
 
 */
 
-enum class ColorComp { red, green, blue };
+enum class ColorChannel { red, green, blue };
 
-struct Pixel
+struct PixelColor
 {
     unsigned char red;
     unsigned char green;
     unsigned char blue;
 
-    unsigned char operator()(ColorComp cc) const { 
+    unsigned char operator()(ColorChannel cc) const { 
         switch(cc) {
-            case ColorComp::red:   return red;
-            case ColorComp::green: return green;
-            case ColorComp::blue:  return blue;
+            case ColorChannel::red:   return red;
+            case ColorChannel::green: return green;
+            case ColorChannel::blue:  return blue;
         }
     }
 };
@@ -108,7 +135,7 @@ class PixelImage
     private:
         size_t height;
         size_t width;
-        std::vector<Pixel> data;
+        std::vector<PixelColor> data;
 
     public:
 
@@ -118,27 +145,43 @@ class PixelImage
         
         ~PixelImage() {}
 
-        size_t getheight(){ return height; };
+        size_t getheight() const { return height; };
 
-        size_t getwidth(){ return width; };
+        size_t getwidth() const { return width; };
 
-        Pixel& operator()( size_t row, size_t col ) {
-            assert(row >= 0 && row < height);
-            assert(col >= 0 && col < width);
+        PixelColor& operator()( size_t row, size_t col ) {
+            assert( row >= 0 && row < height && col >= 0 && col < width );
             return data.at( row * width + col );
         }
         
-        Pixel operator()( size_t row, size_t col ) const {
-            assert(row >= 0 && row < height);
-            assert(col >= 0 && col < width);
+        PixelColor operator()( size_t row, size_t col ) const {
+            assert( row >= 0 && row < height && col >= 0 && col < width );
             return data.at( row * width + col );
+        }
+
+        auto get_interpolated_function() const {
+            return [&]( double x, double y, ColorChannel cc ) -> double
+            {
+                const PixelImage& pixelimage = *this;
+                
+                double ny = floor( y * pixelimage.getwidth()  );
+                double nx = floor( x * pixelimage.getheight() );
+                double lx = x - nx;
+                double ly = y - ny;
+
+                PixelColor p00 = pixelimage( ny+0, nx+0 );
+                PixelColor p01 = pixelimage( ny+0, nx+1 );
+                PixelColor p10 = pixelimage( ny+1, nx+0 );
+                PixelColor p11 = pixelimage( ny+1, nx+1 );
+
+                return lx * ly * p00(cc) + (1-lx) * ly * p01(cc) + lx * (1-ly) * p10(cc) + (1-lx) * (1-ly) * p11(cc);
+            };
         }
 };
 
 
 PixelImage readPixelImage( std::string str )
 {
-    
     InitializeMagick(nullptr);
 
     Image image;    
@@ -149,43 +192,53 @@ PixelImage readPixelImage( std::string str )
 
     PixelImage ret( height, width ); 
     
-    // Get the pixel values and store them
-    Pixels view(image);
-    const PixelPacket* pixel_cache = view.getConst(0, 0, width, height);
-    for (size_t row = 0; row < height; row++) 
-    for (size_t col = 0; col < width; col++) 
+    for( size_t row = 0; row < height; row++) 
+    for( size_t col = 0; col < width; col++)
     {
-            size_t index = row * width + col;
-
-            unsigned char red   = pixel_cache[index].red;
-            unsigned char green = pixel_cache[index].green;
-            unsigned char blue  = pixel_cache[index].blue;
-
-            ret( row, col ) = { red, green, blue } ;
+        ColorRGB color = image.pixelColor( row, col ); 
+        ret(row,col).red   = (int)(255*color.red());
+        ret(row,col).green = (int)(255*color.green());
+        ret(row,col).blue  = (int)(255*color.blue());
     }
 
     return ret;
+
+    // Pixels view(image);
+    // const PixelPacket* pixel_cache = view.getConst(0, 0, width, height);
+    // for( size_t row = 0; row < height; row++) 
+    // for( size_t col = 0; col < width; col++) 
+    // {
+    //         size_t index = row * width + col;
+
+    //         unsigned char red   = pixel_cache[index].red;
+    //         unsigned char green = pixel_cache[index].green;
+    //         unsigned char blue  = pixel_cache[index].blue;
+
+    //         ret( row, col ) = { red, green, blue } ;
+    // }
+
+    // return ret;
 }
 
-
-PixelImage pixelimage(1,1);
-
-auto get_pixel_color = []( double x, double y, ColorComp cc ) -> double
+void savePixelImage( const PixelImage& pim, std::string str )
 {
-    double ny = floor( y * pixelimage.getwidth()  );
-    double nx = floor( x * pixelimage.getheight() );
+    // InitializeMagick(nullptr);
 
-    Pixel p00 = pixelimage( ny+0, nx+0 );
-    Pixel p01 = pixelimage( ny+0, nx+1 );
-    Pixel p10 = pixelimage( ny+1, nx+0 );
-    Pixel p11 = pixelimage( ny+1, nx+1 );
+    size_t width  = pim.getwidth();
+    size_t height = pim.getheight();
 
-    double lx = x - nx;
-    double ly = y - ny;
+    Image image( Geometry(width, height), "white" );
 
-    return lx * ly * p00(cc) + (1-lx) * ly * p01(cc) + lx * (1-ly) * p10(cc) + (1-lx) * (1-ly) * p11(cc);
-};
-
+    for( size_t row = 0; row < height; row++) 
+    for( size_t col = 0; col < width; col++)
+    {
+        auto color = pim( row, col );
+        image.pixelColor( row, col, ColorRGB( color.red/255., color.green/255., color.blue/255. ) ); 
+    }
+        
+    image.write( str ); 
+    return;
+}
 
 
 
@@ -196,51 +249,65 @@ auto get_pixel_color = []( double x, double y, ColorComp cc ) -> double
 
 int main() {
     
-    // Initialize ImageMagick library
-    InitializeMagick(nullptr);
+    auto pim = readPixelImage("lena_color.tiff");
 
-    // Load the image file
-    Image image;
-    
-    image.read("lena_color.tiff");
-
-    // Get the dimensions of the image
-    size_t width = image.columns();
-    size_t height = image.rows();
-
-    // Allocate stack-allocated arrays to store the pixel values
-    unsigned char pixels_r[height][width];
-    unsigned char pixels_g[height][width];
-    unsigned char pixels_b[height][width];
-
-    // Get the pixel values and store them in the arrays
-    Pixels view(image);
-    PixelPacket* pixel_cache = view.get(0, 0, width, height);
-    for (size_t row = 0; row < height; row++) {
-        for (size_t col = 0; col < width; col++) {
-            size_t index = row * width + col;
-            pixels_r[row][col] = pixel_cache[index].red;
-            pixels_g[row][col] = pixel_cache[index].green;
-            pixels_b[row][col] = pixel_cache[index].blue;
-
-            pixel_cache[index].red   = 255 - pixel_cache[index].red;
-            pixel_cache[index].green = 255 - pixel_cache[index].green;
-            pixel_cache[index].blue  = 255 - pixel_cache[index].blue;
-        }
+    for( size_t row = 0; row < pim.getheight(); row++) 
+    for( size_t col = 0; col < pim.getwidth(); col++) 
+    {
+        pim(row,col).red = 255 - pim(row,col).red;
+        pim(row,col).green = 255 - pim(row,col).green;
+        pim(row,col).blue = 255 - pim(row,col).blue;
     }
 
-    view.sync();
+    savePixelImage(pim,"anel_color.tiff");
 
-    image.write("enal_color.tiff");
-
-    // Print the pixel values
-    for (size_t row = 0; row < height; row++) {
-        for (size_t col = 0; col < width; col++) {
-            std::cout << "(" << (int)pixels_r[row][col] << "," << (int)pixels_g[row][col] << "," << (int)pixels_b[row][col] << ") ";
-        }
-        std::cout << std::endl;
-    }
-
-    // Terminate ImageMagick library    
     return 0;
+
+    // // Initialize ImageMagick library
+    // InitializeMagick(nullptr);
+
+    // // Load the image file
+    // Image image;
+    
+    // image.read("lena_color.tiff");
+
+    // // Get the dimensions of the image
+    // size_t width = image.columns();
+    // size_t height = image.rows();
+
+    // // Allocate stack-allocated arrays to store the pixel values
+    // unsigned char pixels_r[height][width];
+    // unsigned char pixels_g[height][width];
+    // unsigned char pixels_b[height][width];
+
+    // // Get the pixel values and store them in the arrays
+    // Pixels view(image);
+    // PixelPacket* pixel_cache = view.get(0, 0, width, height);
+    // for( size_t row = 0; row < height; row++) {
+    //     for( size_t col = 0; col < width; col++) {
+    //         size_t index = row * width + col;
+    //         pixels_r[row][col] = pixel_cache[index].red;
+    //         pixels_g[row][col] = pixel_cache[index].green;
+    //         pixels_b[row][col] = pixel_cache[index].blue;
+
+    //         pixel_cache[index].red   = 255 - pixel_cache[index].red;
+    //         pixel_cache[index].green = 255 - pixel_cache[index].green;
+    //         pixel_cache[index].blue  = 255 - pixel_cache[index].blue;
+    //     }
+    // }
+
+    // view.sync();
+
+    // image.write("enal_color.tiff");
+
+    // // Print the pixel values
+    // for( size_t row = 0; row < height; row++) {
+    //     for( size_t col = 0; col < width; col++) {
+    //         std::cout << "(" << (int)pixels_r[row][col] << "," << (int)pixels_g[row][col] << "," << (int)pixels_b[row][col] << ") ";
+    //     }
+    //     std::cout << std::endl;
+    // }
+
+    // // Terminate ImageMagick library    
+    // return 0;
 }
