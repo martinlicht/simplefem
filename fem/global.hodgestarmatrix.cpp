ec967df08527d0fa5b7c4f0af85692e149761c6e
Martin Licht <martin.licht@epfl.ch>
2025-01-04 22:27:37 +0100

    hodge star stuff

--- fem/global.hodgestarmatrix.cpp
+++ fem/global.hodgestarmatrix.cpp
@@ -17,6 +17,60 @@
 
 
 
+
+
+DenseMatrix EuclideanHodgeStar( int n, int k )
+{
+    assert( n >= 0 );
+    assert( k >= 0 && k <= n );
+    
+    const std::vector<IndexMap>   sigmas_in  = generateSigmas( IndexRange( 1, k   ), IndexRange( 0, n ) );
+    const std::vector<IndexMap>   sigmas_out = generateSigmas( IndexRange( 1, n-k ), IndexRange( 0, n ) );
+
+    assert( sigmas_in.size() == sigmas_out.size() );
+
+    DenseMatrix ret( sigmas_in.size(), sigmas_out.size(), 0. );
+
+    for( int r = 0; r < sigmas_out.size(); r++ )
+    {
+        const auto& sigma_out = sigmas_out[r];
+
+        int c = 0;
+
+        for( ; c < sigmas_in.size(); c++ ) 
+        {
+            auto sigma_in = sigmas_in[c];
+
+            bool common_index = false;
+
+            for( auto v : sigma_out.getvalues() )
+            for( auto w :  sigma_in.getvalues() )
+            {
+                common_index = common_index or ( v == w );
+            }
+
+            if( not common_index ) break;
+        }
+
+        assert( c != sigmas_in.size() );
+
+        const auto& sigma_in = sigmas_in[c];
+
+        int signum = 1.; // determine the specific coefficient needed
+
+        for( int i = 1; i <= k;   i++ )
+        for( int j = 1; j <= n-k; j++ )
+        {
+            if( sigma_in[i] > sigma_out[j] ) signum *= -1;
+        }
+
+        ret(r,c) = signum;
+    }
+
+    return ret;
+}
+
+
 SparseMatrix FEECBrokenHodgeStarMatrix( const Mesh& mesh, int n, int k, int r )
 {
     
@@ -55,12 +109,25 @@ SparseMatrix FEECBrokenHodgeStarMatrix( const Mesh& mesh, int n, int k, int r )
 
     assert( polyMM.is_square() and polyMM.getdimin() == binomial_integer( n+r, n ) );
     
+    /*
+    Method 1:
+
+    1. Develop a QR decomposition of the transformation matrix to obtain local Euclidean coordinate system;
+       In the full-dimensional case, an alternative method is feasible as well (introduce boolean switch).
+    2. Write barycentric derivatives in terms of those coordinates (canonicalize the zeroth gradient)
+    3. Apply Euclidean Hodge star
+    4. Transform back into barycentric coordinate system 
+
+    Method 2:
+    1. Write the mass pairing of the n-k forms 
+    2. Define the Hodge star implicitly.
+    */
+
     #if defined(_OPENMP)
     #pragma omp parallel for
     #endif
     for( int s = 0; s < num_simplices; s++ )
     {
-        
         // obtain geometric information 
         
         Float measure      = mesh.getMeasure( n, s );
